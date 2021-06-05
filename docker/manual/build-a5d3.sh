ce7cd41e47cac98a86bff8adeb99be83babc651d
wang <wangyuchun1969@outlook.com>
2021-06-05 17:00:26 +0800

    sorting

--- docker/manual/build-a5d3.sh
+++ docker/manual/build-a5d3.sh
@@ -3,6 +3,6 @@
 set -ex
 tar -zxf /home/openwrt/openwrt.sources.tgz -C /home/openwrt
 cd /home/openwrt/openwrt
-cp /home/openwrt/targets/config-at91-a5d3 .config
+cp /home/openwrt/devices/at91sama5d3_xplain/.config ./
 make defconfig
 make -j$(nproc) || make V=s
